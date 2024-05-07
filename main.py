b71281e8e425bf13b9a1330164b83e79a2faac62
shimadachi <fark213enes@gmail.com>
2024-05-07 17:11:51 +0300

    guncellendi

---
+++ main.py
@@ -0,0 +1,96 @@
+from site_interaction import Api
+import questionary as qa
+import tkinter as tk
+from tkinter import filedialog
+from os import system
+from rich.console import Console
+from rich.progress import Progress, BarColumn,TextColumn
+import time
+
+qa_style = qa.Style(
+    [
+        ("question", "fg:#cd2adc bold"),
+        ("text", " fg:#23d1e3"),
+        ("answer", "fg:#23d1e3"),
+        ("pointer", "fg:#fd0000"),
+    ]
+)
+
+console = Console()
+
+
+clear_console = lambda: system("cls")
+
+with console.status(
+    "[green] Webdriver Baslatiliyor", spinner="point", spinner_style="white"
+):
+    api = Api()
+
+
+def program(folder):
+
+    api.go_site()
+
+    api.go_series()
+
+    links = api.get_episode_links()
+
+    api.download_series(links, folder)
+
+
+def quit_program():
+    with console.status(
+        "[green] Cikis yapiliyor", spinner="point", spinner_style="white"
+    ):
+        api.driver_quit()
+        quit()
+
+
+folder_selected = None
+def folder_select():
+    if folder_selected == None:
+        with console.status("Indirme yapilacak klasoru secin"):
+            root = tk.Tk()
+            root.withdraw()
+            folder = filedialog.askdirectory()
+
+        if folder == "":
+            raise FileNotFoundError
+        return folder
+    else:
+        pass
+
+
+try:
+    while True:
+        #Cancelled by user degisecek
+        
+        choice = qa.select(
+            message="Secenekler",
+            choices=["Anime/Dizi indir", "Cikis yap"],
+            qmark="",
+            instruction=" ",
+            style=qa_style,
+            
+        ).ask()
+    
+
+        if choice == None:
+            raise KeyboardInterrupt
+
+        if choice == "Anime/Dizi indir":
+            if folder_selected == None:
+                folder_selected = folder_select()
+            else:
+                print(folder_selected)
+            print(f"Indirilecek klasor --> {folder_selected}")
+            program(folder_selected)
+            clear_console()
+
+        if choice == "Cikis yap":
+            quit_program()
+            break
+
+
+except (KeyboardInterrupt, EOFError, FileNotFoundError):
+    quit_program()
--- site_interaction.py
+++ site_interaction.py
@@ -30,15 +30,24 @@ class Api:
 
         self.driver.quit()
 
-    def wait_element(self, by, element, default_wait_delay=10, max_attempt=3):
+
+    # 0 : default located wait
+    # 1 : wait clickable
+    def wait_element(self, by, element, default_wait_delay=10, max_attempt=3,mode = 0):
 
         attempt = 0
         while attempt < max_attempt:
             try:
-                WebDriverWait(self.driver, default_wait_delay).until(
-                    EC.presence_of_element_located((by, element))
-                )
-                break
+                if mode == 0:
+                    WebDriverWait(self.driver, default_wait_delay).until(
+                        EC.presence_of_element_located((by, element))
+                    )
+                    break
+                if mode == 1:
+                    WebDriverWait(self.driver, default_wait_delay).until(
+                        EC.element_to_be_clickable((by, element))
+                    )
+                    break
             except (
                 selenium.common.exceptions.StaleElementReferenceException,
                 selenium.common.exceptions.ElementNotInteractableException,
@@ -48,32 +57,40 @@ class Api:
                 attempt += 1
                 pass
 
-    def wait_element_clickable(self, by, element, default_wait_delay=10, max_attempt=3):
+    # def wait_element_clickable(self, by, element, default_wait_delay=10, max_attempt=3):
 
-        attempt = 0
-        while attempt < max_attempt:
-            try:
-                WebDriverWait(self.driver, default_wait_delay).until(
-                    EC.element_to_be_clickable((by, element))
-                )
-                break
-            except (
-                selenium.common.exceptions.StaleElementReferenceException,
-                selenium.common.exceptions.ElementNotInteractableException,
-                selenium.common.exceptions.NoSuchElementException,
-                selenium.common.exceptions.TimeoutException,
-            ):
-                attempt += 1
-                pass
+    #     attempt = 0
+    #     while attempt < max_attempt:
+    #         try:
+    #             WebDriverWait(self.driver, default_wait_delay).until(
+    #                 EC.element_to_be_clickable((by, element))
+    #             )
+    #             break
+    #         except (
+    #             selenium.common.exceptions.StaleElementReferenceException,
+    #             selenium.common.exceptions.ElementNotInteractableException,
+    #             selenium.common.exceptions.NoSuchElementException,
+    #             selenium.common.exceptions.TimeoutException,
+    #         ):
+    #             attempt += 1
+    #             pass
 
     def go_site(self):
         self.driver.get("https://diziwatch.net/")
 
     def search(self):
+        qa_style = questionary.Style(
+            [
+            ("question", "fg:#cd2adc bold"),
+            ("text", " fg:#23d1e3"),
+            ("answer", "fg:#23d1e3"),
+            ("pointer", "fg:#fd0000"),
+            ]  
+        )
         self.wait_element(By.CSS_SELECTOR, "#searchInput")
         search_input = self.driver.find_element(By.CSS_SELECTOR, "#searchInput")
         search_input.clear()
-        search_input.send_keys(input("Search: ") + Keys.ENTER)
+        search_input.send_keys(input("Ara: ") + Keys.ENTER)
 
         list_series = []
         self.wait_element(By.XPATH, r"//*[@id='search-name']", 1)
@@ -84,7 +101,7 @@ class Api:
         if len(list_series) == 0:
             raise ValueError
         return questionary.select(
-            "Bulunan seriler: ", choices=list_series, qmark=""
+            "Bulunan seriler: ", choices=list_series, qmark="-->", instruction= " ",style=qa_style
         ).ask()
 
     # Istenen serinin sayfasina ulasir
@@ -103,8 +120,8 @@ class Api:
                 self.wait_element(
                     By.XPATH, rf"//div[@id='search-name' and text()='{series}']", 1.5, 1
                 )
-                self.wait_element_clickable(
-                    By.XPATH, rf"//div[@id='search-name' and text()='{series}']", 1.5, 1
+                self.wait_element(
+                    By.XPATH, rf"//div[@id='search-name' and text()='{series}']", 1.5, 1, mode=1
                 )
                 target_series = self.driver.find_element(
                     By.XPATH, rf"//div[@id='search-name' and text()='{series}']"
@@ -157,7 +174,7 @@ class Api:
         ).text
         return self.filter_text(series_name)
 
-    def download_series(self, target_series_episode_links):
+    def download_series(self, target_series_episode_links,path= None):
 
         series_name = self.get_series_name()
 
@@ -178,17 +195,17 @@ class Api:
             ):
                 pass
 
-            self.download_video(series_name, file_name)
+            self.download_video(series_name, file_name,path)
 
     # Kaliteyi ayarlar
     def set_quality_settings(self):
 
-        self.wait_element_clickable(By.CSS_SELECTOR, "#player")
+        self.wait_element(By.CSS_SELECTOR, "#player", mode=1)
 
         video_player = self.driver.find_element(By.CSS_SELECTOR, "#player")
         video_player.click()
 
-        self.wait_element_clickable(By.CSS_SELECTOR, "div.jw-icon:nth-child(15)")
+        self.wait_element(By.CSS_SELECTOR, "div.jw-icon:nth-child(15)",mode=1)
         setting_button = self.driver.find_element(
             By.CSS_SELECTOR, "div.jw-icon:nth-child(15)"
         )
@@ -201,11 +218,12 @@ class Api:
             1,
         )
 
-        self.wait_element_clickable(
+        self.wait_element(
             By.CSS_SELECTOR,
             "#jw-player-settings-submenu-quality > div:nth-child(1)",
             0.5,
             1,
+            mode=1
         )
         video_quality_info_element = self.driver.find_element(
             By.CSS_SELECTOR,
@@ -238,17 +256,17 @@ class Api:
         else:
             ###Secenek sun sectir
             season_opinion = questionary.select(
-                "Sezonlar : ", choices=list(season_list.keys())
+                "Sezonlar : ", choices=list(season_list.keys()),qmark="-->",instruction = " "
             ).ask()
             season_list[season_opinion].click()
 
-    def download_video(self, series_name, file_name, path=r"D:\emby_video"):
+    def download_video(self, series_name, file_name, path= None):
         self.wait_element(By.CSS_SELECTOR, ".jw-video")
         video_element = self.driver.find_element(By.CSS_SELECTOR, ".jw-video")
         src = video_element.get_attribute("src")
         self.driver.get("about:blank")
         yt_dlp.std_headers["Referer"] = "https://diziwatch.net/"
-        URLS = src
+        URL = src
         ydl_opts = {
             "outtmpl": rf"{path}\{series_name}/{file_name}.%(ext)s",
             "ignoreerrors": True,
@@ -256,9 +274,9 @@ class Api:
             "logger": Logger(),
         }
         with Progress() as self.progress:
-            self.downloading = self.progress.add_task("[red]Indiriliyor", total=100)
+            self.downloading = self.progress.add_task("[red]Indiriliyor", total=100,)
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                ydl.download(URLS)
+                ydl.download(URL)
             self.progress.remove_task(self.downloading)
 
     # with Progress() as progress:
@@ -270,7 +288,7 @@ class Api:
             percentage = float(re.search(rf"([0-9](?:[0-9]?).[0-9]%)", percentage_raw)[0].strip("%"))
             self.progress.update(self.downloading, completed=percentage)
         if d["status"] == "finished":
-            print(f"Completed --> {d["filename"]}")
+            print(f"Indirme Tamamlandi --> {d["filename"]}")
 
 
 class Logger(object):
